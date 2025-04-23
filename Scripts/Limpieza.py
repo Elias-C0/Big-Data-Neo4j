d1054c224f49f03c6f8fe55384282236e1e0ef73
Adriel <adrielthu@hotmail.com>
2025-04-23 11:24:52 -0300

    Agregando comentarios

--- Scripts/Limpieza.py
+++ Scripts/Limpieza.py
@@ -2,30 +2,33 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
-# Cargar las variables desde config.env
+# Carga las variables desde config.env
 load_dotenv("Scripts/config.env")
 
-# Obtener las rutas desde las variables de entorno
-input_file = os.getenv("INPUT_FILE")  # Ruta del archivo de entrada
-output_file = os.getenv("OUTPUT_FILE")  # Ruta del archivo de salida
+# Obtiene las rutas desde las variables de entorno
+input_file = os.getenv("INPUT_FILE")  # Archivo de entrada
+output_file = os.getenv("OUTPUT_FILE")  # Achivo de salida
 
 
 df_playlist = pd.read_csv(
     input_file,
-    on_bad_lines="skip"  # Ignora líneas problemáticas
+    on_bad_lines="skip"  # Ignora líneas
 )
 
-df_playlist.columns = df_playlist.columns.str.replace('"', '')
-df_playlist.columns = df_playlist.columns.str.replace(' ', '')
+# Limpia los nombres de las columnas del DataFrame
+df_playlist.columns = df_playlist.columns.str.replace('"', '')  # Elimina comillas doble
+df_playlist.columns = df_playlist.columns.str.replace(' ', '')  # Elimina espacios
 df_playlist.columns
 
-df_playlist['trackname'] = df_playlist['trackname'].str.replace(r'\s*[\'"“”]+\s*', ' ', regex=True)
-
+# Limpia los valores de la columna 'trackname'
+df_playlist['trackname'] = df_playlist['trackname'].str.replace(
+    r'\s*[\'"“”]+\s*', ' ', regex=True  # Reemplaza comillas simples, dobles y otros caracteres similares por un espacio
+)
 df_playlist.to_csv(
     output_file,
     index=False,  # Evita guardar el índice como una columna en el CSV
-    sep=",",  # Especifica el delimitador, en este caso una coma
-    encoding="utf-8"  # Asegura un correcto manejo de caracteres especiales
+    sep=",",  # Especifica el delimitador
+    encoding="utf-8"
 )
 
 print("Archivo creado!!")
